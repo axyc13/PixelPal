262d7e6a4573024ddc2746e6f5ad5f3102db2eaf
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-08-02 17:35:22 +1200

    feat: created landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './global.css'
+import googleLogo from './assets/google.png'
 
 function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center h-screen bg-[#EBE9D2]">
-        <h1 className="text-9xl font-04b text-[#3B23BB]">
-          PixelPals
+        <h1 className="text-9xl font-04b text-[#3B23BB] pb-25">
+          PIXELPAL
         </h1>
-        <button>SIGN IN WITH GOOGLE</button>
-
+        <div className = "bg-[#7B63FF] text-[#fff] font-pixel p-3 rounded border-2 border-black pr-5 pl-5">
+          <img src={googleLogo} className="w-6 h-6 inline mr-2" alt="Google Logo" />
+          <button >Sign Up With Google</button>
+        </div>
       </div>
     </>
   )
