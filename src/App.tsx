baaac3074dd22861010a0d49b271ec28f6e60d58
Andrew Chen <ache337@aucklanduni.ac.nz>
2025-08-02 14:57:49 +1200

    SolvedIssue

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <h1 className="text-3xl font-bold underline">
           Hello world!
         </h1>
-        <p className="text-lg text-gray-700">
+        <p className="text-lg text-red-100">
           This is a simple React app with Tailwind CSS.
         </p>
       </div>
